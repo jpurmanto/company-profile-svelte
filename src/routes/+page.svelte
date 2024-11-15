15b8a7af152e97a9aa8089b658cb1a8d1c113abe
aalfath7 <abdielhafizhy@gmail.com>
2024-11-15 14:56:17 +0700

    add carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
-
+  import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+  import '@splidejs/svelte-splide/css';
 </script>
 
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<Splide aria-label="My Favorite Images">
+  <SplideSlide>
+    <img src="src/night_at_the_lake_by_notthatcherry_dfvdp21.png" alt="Image 1"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="src/night_walk_of_dream_by_notthatcherry_dfvp5uf.png" alt="Image 2"/>
+  </SplideSlide>
+</Splide>
 
 <style lang="postcss">
   :global(html) {
